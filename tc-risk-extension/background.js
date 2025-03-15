328a4e9a36b0f39bdaef8bfbf464118b95690d03
sivanimohan <98155411+sivanimohan@users.noreply.github.com>
2025-03-16 00:37:57 +0530

    Update background.js

--- tc-risk-extension/background.js
+++ tc-risk-extension/background.js
@@ -1,13 +1,3 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "analyze") {
-    fetch("https://t-criskdetector.onrender.com", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text: request.data })
-    })
-      .then(response => response.json())
-      .then(data => {
-        chrome.runtime.sendMessage({ action: "result", result: data });
-      });
-  }
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("T&C Risk Detector installed");
 });
